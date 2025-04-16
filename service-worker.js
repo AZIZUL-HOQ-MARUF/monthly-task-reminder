f98864e7f91b1155891b4c86be63ce51e7c18c77
Azizul Hoq <azizulmaruf@gmail.com>
2025-04-16 12:32:14 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "task-reminder-cache-v2";
+const CACHE_NAME = "task-reminder-cache-v3";
 const BASE_PATH = self.location.pathname.replace(/\/service-worker\.js$/, "/");
 
 const urlsToCache = [
